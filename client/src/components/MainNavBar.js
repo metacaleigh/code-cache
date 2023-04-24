bdd2ac6ec639710113c66d4bfae29c0ea2b1ead4
caleigh <caleighsteill@outlook.com>
2023-04-24 14:17:13 -0700

    mobile accessible navbar

--- client/src/components/MainNavBar.js
+++ client/src/components/MainNavBar.js
@@ -23,6 +23,35 @@ function NavBar({ editClicked, setEditClicked, showAddFolder, setShowAddFolder }
     <>
       <div className="navbar bg-base-100">
         <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+            <li>                
+              <NavLink to='/blogs'>
+                <a>
+                  Blog
+                </a>  
+                </NavLink>
+                </li>
+            {/* <li tabIndex={0}>
+              <a className="justify-between">
+                Parent
+                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
+              </a>
+              <ul className="p-2">
+                <li><a>Submenu 1</a></li>
+                <li><a>Submenu 2</a></li>
+              </ul>
+            </li> */}
+            <li>
+              <NavLink to="/about-us">
+              <a>About Us</a>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
           <a className="btn btn-ghost normal-case text-xl">
             <img src={CCwhitelogo} alt="code/cache logo" />
           </a>
